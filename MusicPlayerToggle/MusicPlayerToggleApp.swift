4db6a6c56335ff22aa49ef7c86cf66242421a47b
iamkimbert <iamkimbert@gmail.com>
2022-10-13 07:05:12 -0400

    Initial Commit

---
+++ MusicPlayerToggle/MusicPlayerToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerToggleApp.swift
+//  MusicPlayerToggle
+//
+//  Created by Kimberly Townsend on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
